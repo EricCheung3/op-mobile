2402568fe3f7730fcf6f037bb9eb874365f57bf7
Yuan Ji <jiwhiz@gmail.com>
2016-01-25 13:56:47 -0700

    comment out upload ocr result code

--- www/app/upload/upload.controller.js
+++ www/app/upload/upload.controller.js
@@ -45,14 +45,16 @@
             console.log("Successfully uploaded item "+item.alias+", filename "+item.file.name);
             console.log("Status:"+status);
             console.log("response:"+response);
-
-            if (status === 201 && response !== null && response.length > 0) {
-                // upload ocr result
-                var ocrItem = vm.uploader.queue[1];
-                ocrItem.url = vm.receiptsUrl + "/" + response + "/hackload";
-                console.log("Hackload OCR to :"+ocrItem.url);
-                ocrItem.upload();
-            }
+            //
+            // if (status === 201 && response !== null && response.length > 0) {
+            //     // upload ocr result
+            //     var ocrItem = vm.uploader.queue[1];
+            //     if (ocrItem) {
+            //         ocrItem.url = vm.receiptsUrl + "/" + response + "/hackload";
+            //         console.log("Hackload OCR to :"+ocrItem.url);
+            //         ocrItem.upload();
+            //     }
+            // }
         };
     };
 
