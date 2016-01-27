a48228639f9568fdf1a45b32705975b6d237b9fe
Yuan Ji <jiwhiz@gmail.com>
2016-01-26 18:35:32 -0700

    set Content-Security-Policy and clean up

--- www/app/app.run.js
+++ www/app/app.run.js
@@ -9,9 +9,6 @@
 
     function run(   $log,   $rootScope,   $ionicPlatform,   $state,   $stateParams,   apiService,   EnvironmentConfig,   ngFB) {
 
-        //fb login
-        ngFB.init({appId: '710014625767261'});
-
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
 
@@ -20,9 +17,9 @@
         $ionicPlatform.ready(function() {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
+            // if (window.cordova && window.cordova.plugins.Keyboard) {
+            //     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            // }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
